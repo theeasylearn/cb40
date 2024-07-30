26c573e0965bc1707b0d88cb8ad64d12254909bb
MahmadHadi <nayanihadi97@gmail.com>
2024-07-30 19:01:16 +0530

    30 jul

---
+++ scanf.c
@@ -0,0 +1,10 @@
+// output -> printf 
+// input -> scanf 
+#include <stdio.h>
+void main() {
+    int num = 10; // num = 10
+    printf("Enter a number : "); // num = 10
+    scanf("%d", &num); // num = input 
+
+    printf("num = %d ", num);
+}
