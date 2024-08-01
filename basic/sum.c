36758875ceee2e0b5f724f088c05d2874df1f60d
MahmadHadi <nayanihadi97@gmail.com>
2024-08-01 19:01:21 +0530

    1 aug

---
+++ basic/sum.c
@@ -0,0 +1,13 @@
+// ans = num1 + num2;
+#include <stdio.h>
+void main() {
+    int num1, num2;
+
+    printf("Enter number 1 : ");
+    scanf("%d", &num1);
+
+    printf("Enter number 2 : ");
+    scanf("%d", &num2);
+
+    printf("ans = %d ", num1 + num2);
+}
